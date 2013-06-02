27fe19dc27d75291e88b31b39e619b5aafa7acf6
Alex Hornung <alex@alexhornung.com>
2013-06-02 14:22:59 +0100

    more cleanup

--- boot1/boot1.c
+++ boot1/boot1.c
@@ -66,20 +66,71 @@ struct pbkdf_prf prfs[] = {
 };
 
 int
+try_decrypt(char *passphrase)
+{
+	struct xts_ctx *xts_ctx;
+	struct pbkdf_prf *prf;
+	int dklen = 64;
+	uint8_t *dk;
+	uint32_t sector = 0;
+	uint32_t crc;
+	int ret = -1;
+
+	dk = malloc(dklen);
+	xts_ctx = malloc(sizeof(struct xts_ctx));
+
+	for (prf = &prfs[0]; prf->hmac_fn != NULL; ++prf) {
+		/* We decrypt in place, so we have to re-read every time */
+		bios_read_sectors(biosdev, &hdr, 63, 1);
+
+		pbkdf2(dk, dklen, passphrase, strlen(passphrase), hdr.enc.salt,
+		    SALT_LEN, prf->iterations, prf->hmac_fn, prf->digest_sz);
+
+#ifdef DEBUG
+		bios_print_hex(dk, dklen);
+		bios_print("\r\n");
+#endif
+
+		xts_init(xts_ctx, aes256_init, aes256_encrypt_ecb,
+		    aes256_decrypt_ecb, 16, dk, dklen);
+		xts_decrypt(xts_ctx, hdr.enc.enc, sizeof(hdr.enc.enc), &sector,
+		    sizeof(sector));
+		xts_uninit(xts_ctx);
+
+		if (memcmp(hdr.dec.tc_str, TC_SIG, sizeof(hdr.dec.tc_str)) != 0) {
+			bios_print("Signature mismatch\r\n");
+			continue;
+		}
+
+		bswap_inplace(&hdr.dec.tc_ver, sizeof(hdr.dec.tc_ver));
+		bswap_inplace(&hdr.dec.crc_keys, sizeof(hdr.dec.crc_keys));
+		bswap_inplace(&hdr.dec.off_mk_scope, sizeof(hdr.dec.off_mk_scope));
+		bswap_inplace(&hdr.dec.sz_mk_scope, sizeof(hdr.dec.sz_mk_scope));
+		bswap_inplace(&hdr.dec.flags, sizeof(hdr.dec.flags));
+
+		crc = crc32((void *)hdr.dec.keys, sizeof(hdr.dec.keys));
+		if (crc != hdr.dec.crc_keys) {
+			bios_print("Keys CRC mismatch\r\n");
+			continue;
+		}
+
+		ret = 0;
+		break;
+	}
+
+	free (dk, dklen);
+	free (xts_ctx, sizeof(struct xts_ctx));
+
+	return ret;
+}
+
+int
 main()
 {
 	char *passphrase;
-	uint8_t *dk;
 	int r;
-	int iterations = 1000;
-	int dklen = 64;
-	struct xts_ctx *xts_ctx;
-	uint32_t sector = 0;
-	uint32_t crc;
 
-	dk = malloc(dklen);
 	passphrase = malloc(65);
-	xts_ctx = malloc(sizeof(struct xts_ctx));
 
 	bios_print("tcplay boot1\r\n");
 	bios_print("Enter Passphrase: ");
@@ -92,39 +143,15 @@ main()
 		passphrase[r] = '\0';
 		bios_print("\r\n");
 
-		bios_read_sectors(biosdev, &hdr, 63, 1);
 
-		pbkdf2(dk, dklen, passphrase, strlen(passphrase), hdr.enc.salt,
-		    SALT_LEN, iterations, sha512_hmac, SHA512_DIGEST_SZ);
+		r = try_decrypt(passphrase);
 
 		free(passphrase, 65);
 
-		bios_print_hex(dk, dklen);
-		bios_print("\r\n");
-
-		xts_init(xts_ctx, aes256_init, aes256_encrypt_ecb,
-		    aes256_decrypt_ecb, 16, dk, dklen);
-		xts_decrypt(xts_ctx, hdr.enc.enc, sizeof(hdr.enc.enc), &sector, sizeof(sector));
-		xts_uninit(xts_ctx);
-
-		if (memcmp(hdr.dec.tc_str, TC_SIG, sizeof(hdr.dec.tc_str)) != 0) {
-			bios_print("Signature mismatch\r\n");
-			return -1;
-		}
-
-		bswap_inplace(&hdr.dec.tc_ver, sizeof(hdr.dec.tc_ver));
-		bswap_inplace(&hdr.dec.crc_keys, sizeof(hdr.dec.crc_keys));
-		bswap_inplace(&hdr.dec.off_mk_scope, sizeof(hdr.dec.off_mk_scope));
-		bswap_inplace(&hdr.dec.sz_mk_scope, sizeof(hdr.dec.sz_mk_scope));
-		bswap_inplace(&hdr.dec.flags, sizeof(hdr.dec.flags));
-
-		crc = crc32((void *)hdr.dec.keys, sizeof(hdr.dec.keys));
-		if (crc != hdr.dec.crc_keys) {
-			bios_print("Keys CRC mismatch\r\n");
-			return -1;
-		}
-
-		bios_print("Header successfully decrypted\r\n");
+		if (r < 0)
+			bios_print("Wrong password or not a TrueCrypt volume\r\n");
+		else
+			bios_print("Header successfully decrypted\r\n");
 	}
 
 	return 0;
